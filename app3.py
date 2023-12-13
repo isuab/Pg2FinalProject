97cd8ed1a1dacc55ee2202edacec195eb5095d20
isuab <isa23@georgetown.edu>
2023-12-12 21:24:03 -0500

    Third push

--- app3.py
+++ app3.py
@@ -1,3 +1,9 @@
+import pandas as pd 
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+import statsmodels.api as sm 
 
 
 ## Initialize variables
